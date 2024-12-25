7b7a22d426c1ef6d7bdca79f55770e1d70816409
ELEBR0N\elena <se200106d@student.etf.bg.ac.rs>
2024-12-25 13:03:03 +0100

    producer-consumer to kafka, data as batch

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,61 +1,86 @@
 package org.example.application
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.streaming.DataStreamReader
 import org.apache.spark.sql.types._
 
 object Main {
   def main(args: Array[String]): Unit = {
 
-    val spark = SparkSession.builder
-      .appName("SparkKafkaStructuredStreaming")
-      .master("local[*]") // Use appropriate master in production
-      .getOrCreate()
-
-    import spark.implicits._
-
-    val kafkaBootstrapServers = "localhost:9092" // Replace with your Kafka bootstrap servers
-    val inputTopic = "input-topic"
-    val outputTopic = "output-topic"
-
-    val kafkaInputDF = spark.readStream
-      .format("kafka")
-      .option("kafka.bootstrap.servers", kafkaBootstrapServers)
-      .option("subscribe", inputTopic)
-      .option("startingOffsets", "earliest") // Options: earliest, latest, specific offsets
-      .load()
-
-
-    val kafkaData = kafkaInputDF
-      .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
-
-    case class Record(id: Int, name: String, timestamp: String)
+    val yellowTripDataPath = "C:\\Users\\elena\\Desktop\\NYCdata\\yellow_tripdata_2024-02.parquet"
 
     val schema = StructType(
       Seq(
-        StructField("id", IntegerType, true),
-        StructField("name", StringType, true),
-        StructField("timestamp", StringType, true)
+        StructField("total_amount", DoubleType, true),
+        StructField("trip_distance", DoubleType, true)
       )
     )
 
-    val parsedDF = kafkaData
-      .select(from_json($"value", schema).as("data"))
-      .select("data.*")
+    val spark = SparkSession.builder
+      .appName("SparkKafkaStructuredStreaming")
+      .master("local[2]") // Use appropriate master in production
+      .getOrCreate()
 
-    val filteredDF = parsedDF.filter($"id" > 100)
+    val yellow_tripdata: DataFrame = spark.read
+      .schema(schema)
+      .parquet(yellowTripDataPath)
 
-    val outputDF = filteredDF.selectExpr(
-      "CAST(id AS STRING) AS key",
-      "CAST(name AS STRING) AS value"
-    )
+    val yellow_tripdata_simple = yellow_tripdata
+      .select("total_amount", "trip_distance")
 
-    val query = outputDF.writeStream
+    yellow_tripdata_simple.show()
+
+    import spark.implicits._
+
+    val kafkaBootstrapServers = "localhost:29092"
+    val inputTopic = "input_topic"
+    val outputTopic = "output_topic"
+
+    val yellow_data = yellow_tripdata_simple
+      .withColumn("key", lit("key"))
+      .withColumn("value", struct($"total_amount", $"trip_distance"))
+
+    val outputDF = yellow_data
+      .selectExpr("CAST(key AS STRING) AS key", "CAST(value AS STRING) AS value")
+
+    val query = outputDF.write
       .format("kafka")
       .option("kafka.bootstrap.servers", kafkaBootstrapServers)
       .option("topic", outputTopic)
-      //.option("checkpointLocation", "/path/to/checkpoint/dir") // Necessary for fault tolerance
+      .save()
+
+
+
+    val kafkaStream = spark.read
+      .format("kafka")
+      .option("kafka.bootstrap.servers", kafkaBootstrapServers)
+      .option("subscribe", outputTopic)
+      .option("startingOffsets", "earliest")
+      .load()
+
+    val newData = kafkaStream
+      .selectExpr("CAST(key AS STRING) AS key", "CAST(value AS STRING) AS value")
+
+    newData.show(false)
+
+    val parsedData = newData
+      .withColumn("total_amount", split(regexp_replace($"value", "[{}]", ""), ",").getItem(0).cast(DoubleType))
+      .withColumn("trip_distance", split(regexp_replace($"value", "[{}]", ""), ",").getItem(1).cast(DoubleType))
+      //.select(from_json($"value", schema).as("data"))   // value is not json!
+      //.select("data.total_amount", "data.trip_distance")
+
+    parsedData.show()
+
+    /*parsedData.writeStream
+      .outputMode("append")
+      .format("console")
       .start()
-    query.awaitTermination()
+      .awaitTermination()*/
+
+
+
+
   }
+
 }
